55e64f825695d8686efec97abc9609616299529d
Mentor Palokaj <mentor@palokaj.co>
2017-07-10 20:41:24 -0500

    href

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@
 import styles from '../css/styles.scss'
 
 if (document.getElementById('store')) window.location.href = 'https://sandbox-store.squarespace.com/'
+if (document.getElementById('oncommunity')) window.location.href = 'https://sandbox-store.squarespace.com/shop/on-community-a-modern-manifesto'
