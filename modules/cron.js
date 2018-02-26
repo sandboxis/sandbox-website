339bff1264848304c48ae15ca3f855f938ef6ba2
Mentor <mentor@palokaj.co>
2018-02-26 11:31:06 +0100

    Cron done

--- modules/cron.js
+++ modules/cron.js
@@ -1,6 +1,10 @@
 const updateMembers = require( `${__dirname}/import-mailchimp.js` )
 
+console.log( process.argv[2] == 'once' ? 'Updating members once' : 'Started member update cron' )
+
 // seconds * minutes hours
-let frequency = 60 * 60 * 12
-if ( !process.argv[2] == 'once' ) setInterval( frequency, updateMembers )
-else updateMembers(  )
+let frequency = 60 * 60 * 1
+if ( !process.argv[2] == 'once' )
+	setInterval( frequency, updateMembers )
+else
+	updateMembers( )
