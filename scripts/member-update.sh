2746d5fa33fc4606c9bacda703f118b05637c334
gitname <gitmail>
2018-05-09 12:40:09 +0200

    Add some loggers

--- scripts/member-update.sh
+++ scripts/member-update.sh
@@ -1,12 +1,15 @@
 echo 'Pulling remote changes'
+logger "$(date) Sandbox Sync: Pulling changes"
 git pull
 mkdir -p docs/assets
 mkdir -p docs/js
 echo 'Generate new members json'
+logger "$(date) Sandbox Sync: Generate member json"
 node modules/cron.js once
 echo 'Copy member json to docs folder'
 cp src/assets/members.json docs/assets/members.json
 echo 'Add, commit and push the changes'
+logger "$(date) Sandbox Sync: Add, commit & push changes"
 git add -f docs/assets/members.json
 git commit -am 'Automated member database deployment'
 git push
