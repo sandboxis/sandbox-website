dd95d2e9bbb850c6e432264bb95798bbbe45dfa5
Carlos Mondragon <cadragon@gmail.com>
2019-10-06 11:09:39 -0700

    Builds site on push using github actions
    This commit makes use of github actions to build the site on the
    gituhb provided CI/CD environment.
    
    It deletes the documentation from version control so that we can only
    keep the sources there - cleaner history.
    
    It also deletes the deploy script and moves the logic within the `npm
    run build` command in `package.json`

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p docs/assets
-mkdir -p docs/js
-echo 'sandbox.is' > docs/CNAME
-NODE_ENV=production webpack -p --progress
-git add *
-git add -f docs/*
-git commit -am 'Automated website edits deployment'
-git push
