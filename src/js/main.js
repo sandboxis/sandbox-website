d7c4305cc633aa918823d94ee41bad568d2aec3d
Mentor Palokaj <mentor@palokaj.co>
2021-04-15 14:50:35 +0200

    Links as requested by nettra

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,8 @@ if ( id('store') ) redirect( 'https://sandbox-store.squarespace.com/' )
 if ( id('census') ) redirect( 'https://www.notion.so/2020-Sandbox-Community-Census-e88781767f074d0380cb0277607cc289' )
 if ( id('oncommunity') ) redirect( 'https://sandbox-store.squarespace.com/shop/on-community-a-modern-manifesto' )
 if ( id('communityresources') ) redirect( 'https://www.notion.so/Sandbox-Community-Member-Resources-1f834f39288946ac82b397d244d63161' )
+if ( id('gratitude') ) redirect( 'https://www.notion.so/Sandbox-Journeys-WG-2036dc10a35c4664872b0a8f54cc04af' )
+if ( id('zoom') ) redirect( 'https://us02web.zoom.us/j/8976969435' )
 
 
 
